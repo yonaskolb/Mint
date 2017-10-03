10d99f9d8422d1ad78e3db5891756fb9a53facb1
Yonas Kolb <yonaskolb@users.noreply.github.com>
2017-10-03 17:40:52 +0200

    update formula for 0.4.1

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,8 +1,8 @@
 class Mint < Formula
   desc "Dependency manager that installs and runs Swift command line tool packages"
   homepage "https://github.com/yonaskolb/Mint"
-  url "https://github.com/yonaskolb/Mint/archive/0.4.0.tar.gz"
-  sha256 "8ca53fedbe080d79b95a5fbec9fe4350dc58b9a6e89f8c80dfbd6d638a01f700"
+  url "https://github.com/yonaskolb/Mint/archive/0.4.1.tar.gz"
+  sha256 "38ffac969ca18040f054b195c355cc6ef73c071285e21d125a75533151bf40a2"
   head "https://github.com/yonaskolb/Mint.git"
 
   depends_on :xcode
