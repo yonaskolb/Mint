baf2ecf9301d93b57e4cfa450b89b79fd5d0daff
Yonas Kolb <yonaskolb@gmail.com>
2018-01-06 18:00:58 +0800

    Update brew to 0.7.1

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,8 +1,8 @@
 class Mint < Formula
   desc "Dependency manager that installs and runs Swift command line tool packages"
   homepage "https://github.com/yonaskolb/Mint"
-  url "https://github.com/yonaskolb/Mint/archive/0.7.0.tar.gz"
-  sha256 "dfae2c13dea879edb602c3d1b0e67c81fb8c7f7cd9bae7dd137f8353607f50b2"
+  url "https://github.com/yonaskolb/Mint/archive/0.7.1.tar.gz"
+  sha256 "986dd967b818ee044ed0568135db8be2083ad6c3995fe6281ada9b42b224a2ec"
   head "https://github.com/yonaskolb/Mint.git"
 
   depends_on :xcode
