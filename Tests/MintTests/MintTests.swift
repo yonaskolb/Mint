20351e5b597c31fabef634988ac693188b426fc1
Wolfgang Lutz <wlut@num42.de>
2018-03-23 09:14:36 +0100

    also test sshRepo in testRunCommand

--- Tests/MintTests/MintTests.swift
+++ Tests/MintTests/MintTests.swift
@@ -7,6 +7,7 @@ class MintTests: XCTestCase {
 
     let mint = Mint(path: Path.temporary + "mint", installationPath: Path.temporary + "mint-installs")
     let testRepo = "yonaskolb/simplepackage"
+    let sshTestRepo = "git@github.com:yonaskolb/simplepackage.git"
     let testVersion = "2.0.0"
     let latestVersion = "3.0.0"
     let testCommand = "simplepackage"
@@ -127,22 +128,22 @@ class MintTests: XCTestCase {
     }
 
     func testRunCommand() throws {
-
+      try [testRepo, sshTestRepo].forEach { repo in
         // run a specific version
-        let specificPackage = try mint.run(repo: testRepo, version: testVersion, arguments: [testCommand])
+        let specificPackage = try mint.run(repo: repo, version: testVersion, arguments: [testCommand])
         expectMintVersion(package: specificPackage)
 
         // run an already installed version
-        try mint.run(repo: testRepo, version: testVersion, arguments: [testCommand])
+        try mint.run(repo: repo, version: testVersion, arguments: [testCommand])
 
         // run without arguments
-        try mint.run(repo: testRepo, version: testVersion, arguments: [testCommand])
+        try mint.run(repo: repo, version: testVersion, arguments: [testCommand])
 
         // run with arguments
-        try mint.run(repo: testRepo, version: testVersion, arguments: [testCommand, "--version"])
+        try mint.run(repo: repo, version: testVersion, arguments: [testCommand, "--version"])
 
         // run latest version
-        let latestPackage = try mint.run(repo: testRepo, version: "", arguments: [testCommand])
+        let latestPackage = try mint.run(repo: repo, version: "", arguments: [testCommand])
         expectMintVersion(package: latestPackage)
         XCTAssertEqual(latestPackage.version, latestVersion)
 
@@ -156,6 +157,7 @@ class MintTests: XCTestCase {
 
         // check package list is empty
         XCTAssertTrue(try mint.listPackages().isEmpty)
+      }
     }
 
     func testMintErrors() {
