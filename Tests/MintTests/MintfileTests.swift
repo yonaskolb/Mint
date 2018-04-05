99d0370a90fd776c8bff1a8cbe0d0099809510cf
Wolfgang Lutz <wlut@num42.de>
2018-04-05 07:40:07 +0200

    remove duplicate test

--- Tests/MintTests/MintfileTests.swift
+++ Tests/MintTests/MintfileTests.swift
@@ -25,28 +25,5 @@ class MintfileTests: XCTestCase {
       XCTAssertEqual(mintfile!.version(for: "realm/SwiftLint"), "0.25.0")
       XCTAssertEqual(mintfile!.version(for: "another/Repo"), "")
     }
-  
-  func testMintfileFromFile() {
-    let testBundle = Bundle(for: type(of: self))
-    
-    let path = testBundle.path(
-      forResource: "mintfile",
-      ofType: nil,
-      inDirectory: nil
-    )
-    
-    let mintfile = Mintfile(path: Path(path!))
-    
-    XCTAssertEqual(
-      mintfile!.packageInfos,
-      [
-        PackageInfo(version: "1.8.0", repo: "yonaskolb/xcodegen"),
-        PackageInfo(version: "0.25.0", repo: "realm/SwiftLint")
-      ]
-    )
-    
-    XCTAssertEqual(mintfile!.version(for: "realm/SwiftLint"), "0.25.0")
-    XCTAssertEqual(mintfile!.version(for: "another/Repo"), "")
-  }
 
 }
