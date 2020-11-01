cfc5e1bbdae02bc2c6d5102775ab141eecb6c51d
417-72KI <417.72ki@gmail.com>
2020-11-02 01:31:44 +0900

    make tests also

--- Tests/MintTests/ProcessInfoExtensionTests.swift
+++ Tests/MintTests/ProcessInfoExtensionTests.swift
@@ -2,6 +2,8 @@
 import XCTest
 
 final class ProcessInfoExtensionTests: XCTestCase {
+    #if os(macOS)
+
     #if arch(x86_64)
     func testMachineHardwareName_Intel() {
         XCTAssertEqual(ProcessInfo.processInfo.machineHardwareName, "x86_64")
@@ -12,5 +14,7 @@ final class ProcessInfoExtensionTests: XCTestCase {
     func testMachineHardwareName_AppleSilicone() {
         XCTAssertEqual(ProcessInfo.processInfo.machineHardwareName, "arm64")
     }
+    #endif
+
     #endif
 }
