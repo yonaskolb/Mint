e5a026a458e4654d8e9ca7c6b26945f61186f30d
Armen Mkrtchian <Armenm@users.noreply.github.com>
2021-09-21 23:02:43 -0400

    Update SwiftCLI to v6.0.3 (#218)

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.0"),
-        .package(url: "https://github.com/jakeheis/SwiftCLI.git", from: "6.0.0"),
+        .package(url: "https://github.com/jakeheis/SwiftCLI.git", from: "6.0.3"),
         .package(url: "https://github.com/mxcl/Version.git", from: "1.1.0")
     ],
     targets: [
