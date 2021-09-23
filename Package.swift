70f897014b9b691f66ff0b9c47c1c9bb8430efff
Armen Mkrtchian <Armenm@users.noreply.github.com>
2021-09-23 00:59:49 -0400

    Update PathKit to v1.0.1 (#219)

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "MintKit", targets: ["MintKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.0"),
         .package(url: "https://github.com/jakeheis/SwiftCLI.git", from: "6.0.3"),
         .package(url: "https://github.com/mxcl/Version.git", from: "1.1.0")
